add8b6f9b0e0fa87e7cab2f8284b15106cf3b9c9
Nikhitha214 <shashtinikhitha214@gmail.com>
2024-08-08 10:19:25 +0530

    August 8

--- src/Components/CartContext.js
+++ src/Components/CartContext.js
@@ -1,26 +1,3 @@
-// import React, { createContext, useState } from 'react';
-
-// export const CartContext = createContext();
-
-// export const CartProvider = ({ children }) => {
-//   const [cart, setCart] = useState([]);
-//   const [wishlist, setWishlist] = useState([]);
-
-//   const addToCart = (product) => {
-//     setCart([...cart, product]);
-//   };
-
-//   const addToWishlist = (product) => {
-//     setWishlist([...wishlist, product]);
-//   };
-
-//   return (
-//     <CartContext.Provider value={{ cart, wishlist, addToCart, addToWishlist }}>
-//       {children}
-//     </CartContext.Provider>
-//   );
-// };
-
 import React, { createContext, useState } from 'react';
 
 export const CartContext = createContext();
